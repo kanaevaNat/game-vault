fb26a04805f2a6896dee3ed6d4e0418b62e386c2
kanaevaNat <kanaeva.natashenka@mail.ru>
2025-12-03 23:07:01 +0300

    разработка сетки карточек игр

---
+++ src/entities/games/components/Card.vue
@@ -0,0 +1,74 @@
+<script setup>
+const props = defineProps({
+  game: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="card glass">
+    <div class="card__top">
+      <a href="/" class="card__img">
+        <img :src="game.preview" alt="{{game.name}}">
+      </a>
+    </div>
+    <div class="card__bottom">
+      <div class="card__info">
+        <div class="card__info-title">{{game.name}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card{
+  margin-top: 4rem;
+  width: clamp(200px, 80vw, 250px);
+
+  min-height: 0;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  overflow: hidden;
+
+  &__top{
+    aspect-ratio: 1 / 1;
+    width: 100%;
+    position: relative;
+
+  }
+  &__bottom{
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 1rem;
+  }
+  &__img{
+    display: block;
+    width: 100%;
+    height: 100%;
+    img {
+      position: absolute;
+      top: 0; left: 0; width: 100%; height: 100%;
+      object-fit: cover;
+      display: block;
+    }
+  }
+  &__info{
+    width: 100%;
+
+  }
+  &__info-title{
+    text-align: left;
+    overflow-wrap: break-word;
+    white-space: normal;
+    min-width: 0;
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
---
+++ src/entities/games/components/Grid.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {useGameStore} from "@/entities/games/store.js";
+import {computed, onMounted} from "vue";
+import Card from "@/entities/games/components/Card.vue";
+
+const gameStore = useGameStore();
+const games = computed(() => gameStore.games);
+const loading = computed(() => gameStore.loading);
+
+onMounted(() => {
+  if (games.value.length === 0) {
+    gameStore.fetchGames()
+  }
+})
+</script>
+
+<template>
+  <div class="games-grid">
+    <div v-if="loading" class="">Загрузка</div>
+    <div v-else-if="games.length === 0" class="">Нет игр</div>
+    <Card
+      v-for="game in games"
+      :key="game.id"
+      :game="game"
+    />
+  </div>
+</template>
+
+<style scoped>
+.games-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+  gap: 1.5rem;
+  padding: 80px;
+  align-items: stretch;
+}
+</style>
