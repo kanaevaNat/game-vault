abadd5f38593272ef5bff131db448601fe4405e2
kanaevaNat <kanaeva.natashenka@mail.ru>
2026-01-27 01:25:26 +0300

    исправление бага авторизации (терялось состояние авторизации после обновления страницы)

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ import './shared/styles/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router/router.js';
-
 import vuetify from './plugins/vuetify'
+import { useAdminStore } from '@/entities/admin/store'
 
 import App from './App.vue'
 
+const app = createApp(App)
 const pinia = createPinia()
 
-createApp(App).use(pinia).use(router).use(vuetify).mount('#app')
+app.use(pinia)
+app.use(router)
+app.use(vuetify)
+
+const adminStore = useAdminStore()
+await adminStore.initialize()
+
+app.mount('#app')
